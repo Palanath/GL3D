b44af0fac02882f87b7cff9b2ad8eb08e30980c5
Palanath <pala@dusttoash.org>
2023-07-01 16:10:56 -0500

    Successfully rendered a red triangle

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,27 +9,42 @@ void glfwErrorCallback(int err, const char *desc) {
 	std::cerr << "GLFW Error (" << err << "): " << desc;
 }
 
+void printGLErrors() {
+	for (auto x = glGetError(); x != GL_NO_ERROR; x = glGetError())
+		std::cout << "Error: " << x << std::endl;
+}
+
+void glfwWindowResizeCallback(GLFWwindow *win, int, int) {
+	int w, h;
+	glfwGetFramebufferSize(win, &w, &h);
+	if (w > h)
+		glViewport((w - h) / 2, 0, h, h);
+	else
+		glViewport(0, (h - w) / 2, w, w);
+}
+
 int main() {
 	// Launch GLFW and create a window
+	glfwSetErrorCallback(glfwErrorCallback);
 	if (!glfwInit()) {
 		std::cerr << "Error initializing GLFW." << std::endl;
 		return 0;
 	}
 
 	GLFWwindow *wind = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
-			WINDOW_TITLE, nullptr, nullptr);
+	WINDOW_TITLE, nullptr, nullptr);
 	if (!wind) {
 		std::cerr << "Error creating window." << std::endl;
 		return 0;
 	}
 
 	// Setup window
-	glfwSetErrorCallback(glfwErrorCallback);
 	glfwMakeContextCurrent(wind);
 	if (glewInit() != GLEW_OK) {
 		std::cerr << "Failed to initialize GLEW." << std::endl;
 		return 0;
 	}
+	glfwSetWindowSizeCallback(wind, glfwWindowResizeCallback);
 
 	// OpenGL - General Setup
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -38,27 +53,7 @@ int main() {
 	GLuint vbo, vao;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	float object[] = {
-			-.5, .5, 0,
-			-.5, .5, 1,
-			.5, .5, 0,
-
-			.5, .5, 0,
-			-.5, .5, 1,
-			.5, .5, 1,
-
-			-.5, .5, 0,
-			0, -.5, .5,
-			.5, .5, 0,
-
-			-.5, .5, 1,
-			0, -.5, .5,
-			-.5, .5, 0,
-
-			.5, .5, 1,
-			0, -.5, .5,
-			-.5, .5, 1,
-	};
+	float object[] = { -1, 1, -1, 1, 1, -1, 0, -1, -1 };
 	glBufferData(GL_ARRAY_BUFFER, sizeof object, object, GL_STATIC_DRAW);
 
 	glGenVertexArrays(1, &vao);
@@ -68,19 +63,22 @@ int main() {
 
 	// OpenGL - Shader Setup
 	GLuint vertShader = glCreateShader(GL_VERTEX_SHADER), fragShader =
-			glCreateShader(GL_FRAGMENT_SHADER);
-	const char* src[1] = {R"(#version 330 core
+	glCreateShader(GL_FRAGMENT_SHADER);
+	const char *src[1] =
+			{
+					R"(#version 330 core
 layout(location = 0) in vec3 pos;
 
 out vec3 position;
 
 void main() {
-	position = pos;
-})"};
+	gl_Position = vec4(position = pos, 1);
+})" };
 	glShaderSource(vertShader, 1, src, nullptr);
 	glCompileShader(vertShader);
 
-	src[0] = R"(#version 330 core
+	src[0] =
+			R"(#version 330 core
 in vec3 position;
 
 out vec4 color;
@@ -101,6 +99,7 @@ void main() {
 	while (!glfwWindowShouldClose(wind)) {
 		glfwPollEvents();
 		glDrawArrays(GL_TRIANGLES, 0, sizeof(object) / sizeof(float) / 3);
+		printGLErrors();
 		glfwSwapBuffers(wind);
 	}
 
