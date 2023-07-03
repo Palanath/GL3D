b1c378d0fd1ff58cf1f5a339f9483a4e97b69d9d
Palanath <pala@dusttoash.org>
2023-07-03 01:36:05 -0500

    Add ambient and diffuse lighting

--- src/Main.cpp
+++ src/Main.cpp
@@ -58,39 +58,42 @@ int main() {
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	float object[] = {
-				-.5, .5, 0, 1, 0, 0,
-				-.5, .5, 1, 0, 1, 0,
-				.5, .5, 0,  0, 0, 1,
+				-.5, .5, 0,   1, 0, 0,   0, -1, 0,
+				-.5, .5, 1,   0, 1, 0,   0, -1, 0,
+				.5, .5, 0,    0, 0, 1,   0, -1, 0,
 
-				.5, .5, 0,  0, 0, 1,
-				-.5, .5, 1, 0, 1, 0,
-				.5, .5, 1,  1, 1, 0,
+				.5, .5, 0,    0, 0, 1,   0, -1, 0,
+				-.5, .5, 1,   0, 1, 0,   0, -1, 0,
+				.5, .5, 1,    1, 1, 0,   0, -1, 0,
 
-				-.5, .5, 0, 1, 0, 0,
-				0, -.5, .5, 1, 1, 1,
-				.5, .5, 0,  0, 0, 1,
+				-.5, .5, 0,   1, 0, 0,   0, -.5, -1,
+				0, -.5, .5,   1, 1, 1,   0, -.5, -1,
+				.5, .5, 0,    0, 0, 1,   0, -.5, -1,
 
-				-.5, .5, 1, 0, 1, 0,
-				0, -.5, .5, 1, 1, 1,
-				-.5, .5, 0, 1, 0, 0,
+				-.5, .5, 1,   0, 1, 0,   -1, -.5, 0,
+				0, -.5, .5,   1, 1, 1,   -1, -.5, 0,
+				-.5, .5, 0,   1, 0, 0,   -1, -.5, 0,
 
-				.5, .5, 1,  1, 1, 0,
-				0, -.5, .5, 1, 1, 1,
-				-.5, .5, 1, 0, 1, 0,
+				.5, .5, 1,    1, 1, 0,   0, -.5, 1,
+				0, -.5, .5,   1, 1, 1,   0, -.5, 1,
+				-.5, .5, 1,   0, 1, 0,   0, -.5, 1,
 
-				.5, .5, 0,  0, 0, 1,
-				0, -.5, .5, 1, 1, 1,
-				.5, .5, 1,  1, 1, 0
+				.5, .5, 0,    0, 0, 1,   0, .5, 1,
+				0, -.5, .5,   1, 1, 1,   0, .5, 1,
+				.5, .5, 1,    1, 1, 0,   0, .5, 1
 		};
 	glBufferData(GL_ARRAY_BUFFER, sizeof object, object, GL_STATIC_DRAW);
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
-	glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(float) * 6, (void*) 0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, false, sizeof(float) * 6,
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(float) * 9, (void*) 0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, false, sizeof(float) * 9,
+			(void*) (sizeof(float) * 3));
+	glVertexAttribPointer(2, 3, GL_FLOAT, false, sizeof(float) * 9,
 			(void*) (sizeof(float) * 3));
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
 
 	// OpenGL - Shader Setup
 	GLuint vertShader = glCreateShader(GL_VERTEX_SHADER), fragShader =
@@ -99,11 +102,14 @@ int main() {
 			{
 					R"(#version 330 core
 uniform float curtime;
+
 layout(location = 0) in vec3 pos;
 layout(location = 1) in vec3 vertColor;
+layout(location = 2) in vec3 normvec;
 
 out vec3 position;
 out vec3 pointColor;
+out vec3 normalVector;
 
 mat3 zRotation(float rads) {
 	float s = sin(rads), c = cos(rads);
@@ -117,6 +123,7 @@ mat3 yRotation(float rads) {
 
 void main() {
 	gl_Position = vec4(position = yRotation(curtime/5) * zRotation(curtime/5) * pos, 1);
+	normalVector = normvec;
 	pointColor = vertColor;
 })" };
 	glShaderSource(vertShader, 1, src, nullptr);
@@ -124,13 +131,25 @@ void main() {
 
 	src[0] =
 			R"(#version 330 core
+uniform vec3 lightloc;
+uniform vec3 lightColor;
+uniform vec3 ambientLightColor;
+
 in vec3 position;
 in vec3 pointColor;
+in vec3 normalVector;
 
 out vec4 color;
 
 void main() {
-	color = vec4(pointColor.rgb, 1);
+	const float materialAmbientReflectivity = 0.03;
+
+	vec3 pointToLightVector = lightloc - position;
+
+	vec3 ambientTerm = materialAmbientReflectivity * ambientLightColor;
+	float diffuseDotProd = dot(normalize(pointToLightVector), normalize(normalVector));
+	vec3 diffuseTerm = diffuseDotProd < 0 ? vec3(0,0,0) : pointColor * diffuseDotProd * lightColor;
+	color = vec4(ambientTerm + diffuseTerm, 1);
 })";
 	glShaderSource(fragShader, 1, src, nullptr);
 	glCompileShader(fragShader);
@@ -142,6 +161,9 @@ void main() {
 	glUseProgram(shader);
 
 	GLuint curtimeUniform = glGetUniformLocation(shader, "curtime");
+	glUniform3f(glGetUniformLocation(shader, "lightloc"), .8, .3, .25);
+	glUniform3f(glGetUniformLocation(shader, "lightColor"), 1, 1, 1);
+	glUniform3f(glGetUniformLocation(shader, "ambientLightColor"), 1, .7, 0);
 
 	// Render loop
 	double curtime = glfwGetTime();
