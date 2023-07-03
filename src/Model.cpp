03ba11678978ee90ef43fa5a1e379b0494291b51
Palanath <pala@dusttoash.org>
2023-07-03 16:56:33 -0500

    Create Model class

---
+++ src/Model.cpp
@@ -0,0 +1,21 @@
+/*
+ * Model.cpp
+ *
+ *  Created on: Jul 3, 2023
+ *      Author: Palanath
+ */
+
+#include "Model.h"
+
+namespace gl3d {
+
+Model::Model() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Model::~Model() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace gl3d */
---
+++ src/Model.h
@@ -0,0 +1,21 @@
+/*
+ * Model.h
+ *
+ *  Created on: Jul 3, 2023
+ *      Author: Palanath
+ */
+
+#ifndef MODEL_H_
+#define MODEL_H_
+
+namespace gl3d {
+
+class Model {
+public:
+	Model();
+	virtual ~Model();
+};
+
+} /* namespace gl3d */
+
+#endif /* MODEL_H_ */
